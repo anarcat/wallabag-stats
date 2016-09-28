b430957fc2710cf5ef8ed7edaffeb3ff8e678e62
Sven Fischer <git-dev@linux4tw.de>
2016-09-28 23:00:32 +0200

    generate chart only if new data is available

--- stats.go
+++ stats.go
@@ -102,73 +102,72 @@ func main() {
 		// log.Printf("wbgStats: %v\n", wbgStats)
 		writeNewJson(wbgStats)
 
-	}
+		// generate chart
+		graph := chart.Chart{
+			XAxis: chart.XAxis{
+				Name:           "Time",
+				NameStyle:      chart.StyleShow(),
+				Style:          chart.Style{Show: true},
+				ValueFormatter: chart.TimeHourValueFormatter,
+			},
+			YAxis: chart.YAxis{
+				Name:      "Unread",
+				NameStyle: chart.StyleShow(),
+				Style:     chart.Style{Show: true},
+				ValueFormatter: func(v interface{}) string {
+					if vf, isFloat := v.(float64); isFloat {
+						return fmt.Sprintf("%0.0f", vf)
+					}
+					return ""
+				},
+			},
+			YAxisSecondary: chart.YAxis{
+				Name:      "Total",
+				NameStyle: chart.StyleShow(),
+				Style: chart.Style{
+					Show: true, //enables / displays the secondary y-axis
+				},
+				ValueFormatter: func(v interface{}) string {
+					if vf, isFloat := v.(float64); isFloat {
+						return fmt.Sprintf("%0.0f", vf)
+					}
+					return ""
+				},
+			},
+			Background: chart.Style{
+				Padding: chart.Box{
+					Top:  20,
+					Left: 20,
+				},
+			},
+			Series: []chart.Series{
+				chart.TimeSeries{
+					Name:    "Unread",
+					XValues: wbgStats.Times,
+					YValues: wbgStats.Unread,
+				},
+				chart.TimeSeries{
+					YAxis:   chart.YAxisSecondary,
+					Name:    "Total",
+					XValues: wbgStats.Times,
+					YValues: wbgStats.Total,
+				},
+			},
+		}
 
-	// generate chart
-	graph := chart.Chart{
-		XAxis: chart.XAxis{
-			Name:           "Time",
-			NameStyle:      chart.StyleShow(),
-			Style:          chart.Style{Show: true},
-			ValueFormatter: chart.TimeHourValueFormatter,
-		},
-		YAxis: chart.YAxis{
-			Name:      "Unread",
-			NameStyle: chart.StyleShow(),
-			Style:     chart.Style{Show: true},
-			ValueFormatter: func(v interface{}) string {
-				if vf, isFloat := v.(float64); isFloat {
-					return fmt.Sprintf("%0.0f", vf)
-				}
-				return ""
-			},
-		},
-		YAxisSecondary: chart.YAxis{
-			Name:      "Total",
-			NameStyle: chart.StyleShow(),
-			Style: chart.Style{
-				Show: true, //enables / displays the secondary y-axis
-			},
-			ValueFormatter: func(v interface{}) string {
-				if vf, isFloat := v.(float64); isFloat {
-					return fmt.Sprintf("%0.0f", vf)
-				}
-				return ""
-			},
-		},
-		Background: chart.Style{
-			Padding: chart.Box{
-				Top:  20,
-				Left: 20,
-			},
-		},
-		Series: []chart.Series{
-			chart.TimeSeries{
-				Name:    "Unread",
-				XValues: wbgStats.Times,
-				YValues: wbgStats.Unread,
-			},
-			chart.TimeSeries{
-				YAxis:   chart.YAxisSecondary,
-				Name:    "Total",
-				XValues: wbgStats.Times,
-				YValues: wbgStats.Total,
-			},
-		},
-	}
+		graph.Elements = []chart.Renderable{
+			chart.Legend(&graph),
+		}
 
-	graph.Elements = []chart.Renderable{
-		chart.Legend(&graph),
-	}
-
-	buffer := bytes.NewBuffer([]byte{})
-	err = graph.Render(chart.PNG, buffer)
-	if err != nil {
-		panic(err)
-	}
-	err = ioutil.WriteFile("chart.png", buffer.Bytes(), 0644)
-	if err != nil {
-		panic(err)
+		buffer := bytes.NewBuffer([]byte{})
+		err = graph.Render(chart.PNG, buffer)
+		if err != nil {
+			panic(err)
+		}
+		err = ioutil.WriteFile("chart.png", buffer.Bytes(), 0644)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	// log.Printf("time elapsed: %.2fs\n", time.Since(start).Seconds())
