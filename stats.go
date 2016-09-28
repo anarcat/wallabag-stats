c5b565202189acaa549b841ece1c0fbb02dc5e13
Sven Fischer <git-dev@linux4tw.de>
2016-09-28 19:08:11 +0200

    two-axis chart with legend filled with dummy data

--- stats.go
+++ stats.go
@@ -37,12 +37,71 @@ func main() {
 
 	// generate chart
 	graph := chart.Chart{
-		Series: []chart.Series{
-			chart.ContinuousSeries{
-				XValues: []float64{1.0, 2.0, 3.0, 4.0},
-				YValues: []float64{1.0, 2.0, 3.0, 4.0},
+		XAxis: chart.XAxis{
+			Name:           "Time",
+			NameStyle:      chart.StyleShow(),
+			Style:          chart.Style{Show: true},
+			ValueFormatter: chart.TimeHourValueFormatter,
+		},
+		YAxis: chart.YAxis{
+			Name:      "Unread",
+			NameStyle: chart.StyleShow(),
+			Style:     chart.Style{Show: true},
+		},
+		YAxisSecondary: chart.YAxis{
+			Name:      "Total",
+			NameStyle: chart.StyleShow(),
+			Style: chart.Style{
+				Show: false, //enables / displays the secondary y-axis
 			},
 		},
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top:  20,
+				Left: 20,
+			},
+		},
+		Series: []chart.Series{
+			chart.TimeSeries{
+				Name: "Unread",
+				XValues: []time.Time{
+					time.Now().AddDate(0, 0, -10),
+					time.Now().AddDate(0, 0, -9),
+					time.Now().AddDate(0, 0, -8),
+					time.Now().AddDate(0, 0, -7),
+					time.Now().AddDate(0, 0, -6),
+					time.Now().AddDate(0, 0, -5),
+					time.Now().AddDate(0, 0, -4),
+					time.Now().AddDate(0, 0, -3),
+					time.Now().AddDate(0, 0, -2),
+					time.Now().AddDate(0, 0, -1),
+					time.Now(),
+				},
+				YValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0},
+			},
+			chart.TimeSeries{
+				YAxis: chart.YAxisSecondary,
+				Name:  "Total",
+				XValues: []time.Time{
+					time.Now().AddDate(0, 0, -10),
+					time.Now().AddDate(0, 0, -9),
+					time.Now().AddDate(0, 0, -8),
+					time.Now().AddDate(0, 0, -7),
+					time.Now().AddDate(0, 0, -6),
+					time.Now().AddDate(0, 0, -5),
+					time.Now().AddDate(0, 0, -4),
+					time.Now().AddDate(0, 0, -3),
+					time.Now().AddDate(0, 0, -2),
+					time.Now().AddDate(0, 0, -1),
+					time.Now(),
+				},
+				YValues: []float64{3.0, 4.0, 5.0, 7.0, 8.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0},
+			},
+		},
+	}
+
+	graph.Elements = []chart.Renderable{
+		chart.Legend(&graph),
 	}
 
 	buffer := bytes.NewBuffer([]byte{})
