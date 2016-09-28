01d33495ac1cddc0509f8bf504de1592cad91d71
Sven Fischer <git-dev@linux4tw.de>
2016-09-28 17:55:02 +0200

    generate empty chart

--- stats.go
+++ stats.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,6 +10,7 @@ import (
 	"time"
 
 	"github.com/Strubbl/wallabago"
+	"github.com/wcharczuk/go-chart" //exposes "chart"
 )
 
 func main() {
@@ -32,6 +34,20 @@ func main() {
 	log.Printf("unread: %v\n", total-archived)
 	log.Printf("starred: %v\n", starred)
 	log.Printf("time: %v\n", time.Now())
+
+	// generate chart
+	graph := chart.Chart{
+		Series: []chart.Series{
+			chart.ContinuousSeries{
+				XValues: []float64{1.0, 2.0, 3.0, 4.0},
+				YValues: []float64{1.0, 2.0, 3.0, 4.0},
+			},
+		},
+	}
+
+	buffer := bytes.NewBuffer([]byte{})
+	err = graph.Render(chart.PNG, buffer)
+	err = ioutil.WriteFile("chart.png", buffer.Bytes(), 0644)
 
 	log.Printf("time elapsed: %.2fs\n", time.Since(start).Seconds())
 }
