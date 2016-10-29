8da945dfe8d997d5b610fd9a5da6be0a14fbaad1
Sven Fischer <git-dev@linux4tw.de>
2016-10-29 15:57:06 +0200

    remove minus infront of longer name flags

--- flags.go
+++ flags.go
@@ -13,10 +13,10 @@ const defaultDataJSON = "data.json"
 
 var debug = flag.Bool("d", false, "get debug output (implies verbose mode)")
 var v = flag.Bool("v", false, "print version")
-var verbose = flag.Bool("-verbose", false, "verbose mode")
-var chartPNG = flag.String("-chart", defaultChartPNG, "file name to put the chart PNG")
-var configJSON = flag.String("-config", defaultConfigJSON, "file name of config JSON file")
-var dataJSON = flag.String("-data", defaultDataJSON, "file name of data JSON file")
+var verbose = flag.Bool("verbose", false, "verbose mode")
+var chartPNG = flag.String("chart", defaultChartPNG, "file name to put the chart PNG")
+var configJSON = flag.String("config", defaultConfigJSON, "file name of config JSON file")
+var dataJSON = flag.String("data", defaultDataJSON, "file name of data JSON file")
 
 func handleFlags() {
 	flag.Parse()
