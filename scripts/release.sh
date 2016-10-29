75be2d6b6a16ff892540201c36640b073862cad6
Sven Fischer <git-dev@linux4tw.de>
2016-10-29 16:52:26 +0200

    release.sh: test for empty vars

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+curbranch=$(git rev-parse --abbrev-ref HEAD)
+latesttag=$(git describe --abbrev=0 --tags)
+if [ -z "$curbranch" ]; then
+  exit 1
+fi
+if [ -z "$latesttag" ]; then
+  exit 1
+fi
+git checkout "$latesttag"
+
 bin=$(basename $(pwd))
 arch=$(file -b $bin | awk -F , '{ print $2  }' | xargs)
 version=$(grep version version.go | awk -F \" '{ print $2  }')
-curbranch=$(git rev-parse --abbrev-ref HEAD)
-latesttag=$(git describe --abbrev=0 --tags)
+releasefolder="$bin"
 
-git checkout "$latesttag"
-make clean build
-cp "$bin" "$bin-$version"
-strip "$bin-$version"
-7z a "$bin-$version-$arch.7z" "$bin-$version"
-rm "$bin-$version"
+if [ -n "$bin" ]; then
+  make clean build
+  mkdir $releasefolder
+  cp "$bin" "$releasefolder/$bin-$version"
+  strip "$bin-$version"
+  7z a "$bin-$version-$arch.7z" "$releasefolder"
+  rm -r "$releasefolder"
+fi
+
 git checkout "$curbranch"
 
