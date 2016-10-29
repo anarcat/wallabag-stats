662b8be82d774053535d70887bc9402fecd37a83
Sven Fischer <git-dev@linux4tw.de>
2016-10-29 14:44:54 +0200

    move png generation to separate file

---
+++ chart-png.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/wcharczuk/go-chart" //exposes "chart"
+)
+
+const chartPNG = "chart.png"
+
+func generateChartPNG(wbgStats WallabagStats) {
+	// generate chart
+	graph := chart.Chart{
+		XAxis: chart.XAxis{
+			Name:           "Time",
+			NameStyle:      chart.StyleShow(),
+			Style:          chart.Style{Show: true},
+			ValueFormatter: chart.TimeHourValueFormatter,
+		},
+		YAxis: chart.YAxis{
+			Name:      "Unread",
+			NameStyle: chart.StyleShow(),
+			Style:     chart.Style{Show: true},
+			ValueFormatter: func(v interface{}) string {
+				if vf, isFloat := v.(float64); isFloat {
+					return fmt.Sprintf("%0.0f", vf)
+				}
+				return ""
+			},
+		},
+		YAxisSecondary: chart.YAxis{
+			Name:      "Total",
+			NameStyle: chart.StyleShow(),
+			Style: chart.Style{
+				Show: true, //enables / displays the secondary y-axis
+			},
+			ValueFormatter: func(v interface{}) string {
+				if vf, isFloat := v.(float64); isFloat {
+					return fmt.Sprintf("%0.0f", vf)
+				}
+				return ""
+			},
+		},
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top:  20,
+				Left: 20,
+			},
+		},
+		Series: []chart.Series{
+			chart.TimeSeries{
+				Name:    "Unread",
+				XValues: wbgStats.Times,
+				YValues: wbgStats.Unread,
+			},
+			chart.TimeSeries{
+				YAxis:   chart.YAxisSecondary,
+				Name:    "Total",
+				XValues: wbgStats.Times,
+				YValues: wbgStats.Total,
+			},
+		},
+	}
+
+	graph.Elements = []chart.Renderable{
+		chart.Legend(&graph),
+	}
+
+	buffer := bytes.NewBuffer([]byte{})
+	err := graph.Render(chart.PNG, buffer)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(chartPNG, buffer.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"time"
 
 	"github.com/Strubbl/wallabago"
-	"github.com/wcharczuk/go-chart" //exposes "chart"
 )
 
 const lockFile = ".lock"
@@ -57,73 +54,7 @@ func main() {
 
 		// log.Printf("wbgStats: %v\n", wbgStats)
 		writeNewJSON(wbgStats)
-
-		// generate chart
-		graph := chart.Chart{
-			XAxis: chart.XAxis{
-				Name:           "Time",
-				NameStyle:      chart.StyleShow(),
-				Style:          chart.Style{Show: true},
-				ValueFormatter: chart.TimeHourValueFormatter,
-			},
-			YAxis: chart.YAxis{
-				Name:      "Unread",
-				NameStyle: chart.StyleShow(),
-				Style:     chart.Style{Show: true},
-				ValueFormatter: func(v interface{}) string {
-					if vf, isFloat := v.(float64); isFloat {
-						return fmt.Sprintf("%0.0f", vf)
-					}
-					return ""
-				},
-			},
-			YAxisSecondary: chart.YAxis{
-				Name:      "Total",
-				NameStyle: chart.StyleShow(),
-				Style: chart.Style{
-					Show: true, //enables / displays the secondary y-axis
-				},
-				ValueFormatter: func(v interface{}) string {
-					if vf, isFloat := v.(float64); isFloat {
-						return fmt.Sprintf("%0.0f", vf)
-					}
-					return ""
-				},
-			},
-			Background: chart.Style{
-				Padding: chart.Box{
-					Top:  20,
-					Left: 20,
-				},
-			},
-			Series: []chart.Series{
-				chart.TimeSeries{
-					Name:    "Unread",
-					XValues: wbgStats.Times,
-					YValues: wbgStats.Unread,
-				},
-				chart.TimeSeries{
-					YAxis:   chart.YAxisSecondary,
-					Name:    "Total",
-					XValues: wbgStats.Times,
-					YValues: wbgStats.Total,
-				},
-			},
-		}
-
-		graph.Elements = []chart.Renderable{
-			chart.Legend(&graph),
-		}
-
-		buffer := bytes.NewBuffer([]byte{})
-		err = graph.Render(chart.PNG, buffer)
-		if err != nil {
-			panic(err)
-		}
-		err = ioutil.WriteFile("chart.png", buffer.Bytes(), 0644)
-		if err != nil {
-			panic(err)
-		}
+		generateChartPNG(wbgStats)
 	}
 
 	// log.Printf("time elapsed: %.2fs\n", time.Since(start).Seconds())
