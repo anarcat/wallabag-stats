686816232c41ecc54dc87e5aa3d232fd54b056d3
Sven Fischer <git-dev@linux4tw.de>
2016-11-24 19:29:01 +0100

    fix bug where unread equals total
    This bug occurs when the request to get the archived count fails, but
    the call to get total count succeeds. Then our calculated unread count
    is total minus zero, because archived is initialized to zero. In this
    case our unread count is equal to total count. This is bug and a big
    peak in the graph.
    
    The fix checks for unread being equal to total and archvied being zero
    and, most important, last archived count (from the call before) being
    greater than zero.
    
    It is very unlikely that this case happens in a real scenario, because
    the user would have to mark all articles as not read although he read
    them in past. So this would be the reverse process of the main use case
    of wallabag, something like reverse reading or unreading.

--- main.go
+++ main.go
@@ -97,10 +97,19 @@ func main() {
 	}
 
 	// comparing last data set with currently fetched data set
-	// also comparing each item of last data set for not being 0, but current being 0
-	if (wbgStats.Total[len(wbgStats.Total)-1] == total && wbgStats.Unread[len(wbgStats.Unread)-1] == unread && wbgStats.Starred[len(wbgStats.Starred)-1] == starred) || (wbgStats.Total[len(wbgStats.Total)-1] != 0 && total == 0) || (wbgStats.Unread[len(wbgStats.Unread)-1] != 0 && unread == 0) || (wbgStats.Starred[len(wbgStats.Starred)-1] != 0 && starred == 0) {
+	if wbgStats.Total[len(wbgStats.Total)-1] == total && wbgStats.Unread[len(wbgStats.Unread)-1] == unread && wbgStats.Starred[len(wbgStats.Starred)-1] == starred {
 		if *verbose {
 			log.Println("no data change since last call --> nothing to do")
+		}
+		// also comparing each item of last data set for not being 0, but current being 0
+	} else if (wbgStats.Total[len(wbgStats.Total)-1] != 0 && total == 0) || (wbgStats.Unread[len(wbgStats.Unread)-1] != 0 && unread == 0) || (wbgStats.Starred[len(wbgStats.Starred)-1] != 0 && starred == 0) {
+		if *verbose {
+			log.Println("found 0 instead of real value in total, unread or starred, aborting --> nothing to do")
+		}
+		// it is unlikely that we have zero archived items when we had more than zero archived items in our call before
+	} else if unread == total && archived == 0 && wbgStats.Total[len(wbgStats.Total)-1]-wbgStats.Unread[len(wbgStats.Unread)-1] > 0 {
+		if *verbose {
+			log.Println("invalid unread count found, aborting --> nothing to do")
 		}
 	} else {
 		if *verbose {
