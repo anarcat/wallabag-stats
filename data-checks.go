c2c4ed84c6170aac4b42a1018518a2126b993ae2
Sven Fischer <git-dev@linux4tw.de>
2016-11-29 20:04:04 +0100

    test function isDataAllEqualLengths, add test cases to TestIsDataSetNew

--- data-checks.go
+++ data-checks.go
@@ -30,14 +30,20 @@ func isDataSetNew(wbgStats *WallabagStats, total, archived, unread, starred floa
 	if !isDataAllEqualLengths(len(wbgStats.Times), len(wbgStats.Total), len(wbgStats.Unread), len(wbgStats.Starred)) {
 		fmt.Println("error: data set from JSON file is not valid, because array sizes have different length")
 	}
+
+	// check if any of the arrays in wbgStats is zero, so we have a complete new empty array in this case
+	// this check has to be done only after isDataAllEqualLengths(), otherwise it has to be expanded
+	if len(wbgStats.Times) == 0 {
+		return true
+	}
 	// comparing last data set with currently fetched data set
 	if wbgStats.Total[len(wbgStats.Total)-1] == total && wbgStats.Unread[len(wbgStats.Unread)-1] == unread && wbgStats.Starred[len(wbgStats.Starred)-1] == starred {
 		if *verbose {
 			log.Println("no data change since last call --> nothing to do")
 		}
 		return false
-		// also comparing each item of last data set for not being 0, but current being 0
-	} else if (wbgStats.Total[len(wbgStats.Total)-1] != 0 && total == 0) || (wbgStats.Unread[len(wbgStats.Unread)-1] != 0 && unread == 0) || (wbgStats.Starred[len(wbgStats.Starred)-1] != 0 && starred == 0) {
+		// also comparing each item of last data set for not being 0, but current being 0, except for unread, which can go to zero
+	} else if (wbgStats.Total[len(wbgStats.Total)-1] != 0 && total == 0) || (wbgStats.Starred[len(wbgStats.Starred)-1] != 0 && starred == 0) {
 		if *verbose {
 			log.Println("found 0 instead of real value in total, unread or starred, aborting --> nothing to do")
 		}
--- data-checks_test.go
+++ data-checks_test.go
@@ -5,6 +5,37 @@ import (
 	"time"
 )
 
+func TestIsDataAllEqualLengths(t *testing.T) {
+	var tests = []struct {
+		timesCount     int
+		totalCount     int
+		unreadCount    int
+		starredCount   int
+		expectedResult bool
+	}{
+		{0, 0, 0, 0, true},
+		{10000, 10000, 10000, 10000, true},
+		{0, 0, 0, 1, false},
+		{0, 0, 1, 0, false},
+		{0, 1, 0, 0, false},
+		{1, 0, 0, 0, false},
+		{1, 1, 0, 0, false},
+		{1, 1, 1, 0, false},
+		{0, 1, 0, 0, false},
+		{0, 1, 1, 0, false},
+		{0, 1, 1, 1, false},
+		{0, 0, 1, 1, false},
+		{1, 0, 1, 1, false},
+		{1, 1, 0, 1, false},
+	}
+	for _, test := range tests {
+		b := isDataAllEqualLengths(test.timesCount, test.totalCount, test.unreadCount, test.starredCount)
+		if b != test.expectedResult {
+			t.Errorf("isDataAllEqualLengths(%v, %v, %v, %v): expectedResult %v, got %v", test.timesCount, test.totalCount, test.unreadCount, test.starredCount, test.expectedResult, b)
+		}
+	}
+}
+
 func TestIsDataSetNew(t *testing.T) {
 	var tests = []struct {
 		w              WallabagStats
@@ -15,13 +46,29 @@ func TestIsDataSetNew(t *testing.T) {
 		expectedResult bool
 	}{
 		{WallabagStats{Times: []time.Time{}, Total: []float64{}, Unread: []float64{}, Starred: []float64{}}, 1, 1, 0, 0, true},
+		// simulate fresh new wallabag-stats installation and fresh new wallabag installation
 		{WallabagStats{}, 0, 0, 0, 0, true},
+		// simulate fresh new wallabag-stats installation
+		{WallabagStats{}, 500, 100, 400, 20, true},
+		// simulate fresh new wallabag-stats installation
 		{WallabagStats{Times: []time.Time{time.Now()}, Total: []float64{0}, Unread: []float64{0}, Starred: []float64{0}}, 1, 1, 0, 0, true},
+		// simulate corrupt data set in json file
+		{WallabagStats{Times: []time.Time{time.Now()}, Total: []float64{0, 1}, Unread: []float64{0}, Starred: []float64{0}}, 1, 1, 0, 0, false},
+		// simulate all API calls fail
+		{WallabagStats{Times: []time.Time{time.Now()}, Total: []float64{1}, Unread: []float64{1}, Starred: []float64{1}}, 0, 0, 0, 0, false},
+		// simulate deleting one archived item and failing to get starred number from API
+		{WallabagStats{Times: []time.Time{time.Now()}, Total: []float64{100}, Unread: []float64{50}, Starred: []float64{10}}, 99, 49, 50, 0, false},
+		// simulate deleting one archived starred item
+		{WallabagStats{Times: []time.Time{time.Now()}, Total: []float64{100}, Unread: []float64{50}, Starred: []float64{10}}, 99, 49, 50, 9, true},
+		// archived count is now zero although before we already had archived some articles
+		{WallabagStats{Times: []time.Time{time.Now()}, Total: []float64{100}, Unread: []float64{50}, Starred: []float64{10}}, 110, 0, 110, 9, false},
+		// archived count is now equal total count and unread is therefore zero, which is use case "all items read"
+		{WallabagStats{Times: []time.Time{time.Now()}, Total: []float64{100}, Unread: []float64{50}, Starred: []float64{10}}, 110, 110, 0, 9, true},
 	}
 	for _, test := range tests {
 		b := isDataSetNew(&test.w, test.total, test.archived, test.unread, test.starred)
 		if b != test.expectedResult {
-			t.Errorf("isDataSetNew(): expectedResult %v, got %v", test.expectedResult, b)
+			t.Errorf("isDataSetNew(): expectedResult %v, got %v\ntotal=%v, unread=%v, starred=%v, archived=%v\nWallabagStats=%v", test.expectedResult, b, test.total, test.unread, test.starred, test.archived, test.w)
 		}
 	}
 }
