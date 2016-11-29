9baf3c1c4c3db2d93c12141505d4fd402ba2e4de
Sven Fischer <git-dev@linux4tw.de>
2016-11-29 18:49:17 +0100

    write test for function isDataSetNew

---
+++ data-checks_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestIsDataSetNew(t *testing.T) {
+	var tests = []struct {
+		w              WallabagStats
+		total          float64
+		archived       float64
+		unread         float64
+		starred        float64
+		expectedResult bool
+	}{
+		{WallabagStats{Times: []time.Time{}, Total: []float64{}, Unread: []float64{}, Starred: []float64{}}, 1, 1, 0, 0, true},
+		{},
+	}
+	for _, test := range tests {
+		b := isDataSetNew(&test.w, test.total, test.archived, test.unread, test.starred)
+		if b != test.expectedResult {
+			t.Errorf("isDataSetNew(): expectedResult %v, got %v", test.expectedResult, b)
+		}
+	}
+}
